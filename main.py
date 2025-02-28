ebe4708517495ec27d891f55a449871f66c6e477
fcs <francisco.conceicaosilva7@gmail.com>
2025-02-28 10:56:26 +0000

    add info

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from argparse_func import get_argparse_execute_functions
 logs.setup_logs(verbosity=[logging.INFO, logging.INFO - 5, logging.DEBUG, logging.VERBOSE][:4 + 1][-1])
 LOG = logging.getLogger(logs.LOG_BASE_NAME + '.' + __name__)
 
-# TODO: Windows compatibility
-
+# New Ubuntu installation mandatory installs.
+# sudo apt install git, vim
+# sudo snap install pycharm-community --classic
+# sudo snap install nvtop
+# https://github.com/fullstorydev/grpcurl/releases
+#
+# pip3 install psutil, requests
+# for i in {00..26}; do
+#   sudo mkdir -p "/mnt/spacemesh_$i"
+# done
 
 def main():
     LOG.info("Starting Execution")
